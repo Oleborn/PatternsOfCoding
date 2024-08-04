98756ebde1fc3ff7f1db42cd49070db7611c8030
Alexander <alexandergorsc@gmail.com>
2024-08-04 20:38:48 +0300

    add: builder and singleton patterns

---
+++ src/main/java/structuralPatterns/builder/Builder.java
@@ -0,0 +1,9 @@
+package structuralPatterns.builder;
+
+public interface Builder {
+    void setSize(int size);
+    void setPrice(double price);
+    void setName(String name);
+    void setCheese(boolean cheese);
+    Pizza build();
+}
---
+++ src/main/java/structuralPatterns/singleton/Main.java
@@ -0,0 +1,9 @@
+package structuralPatterns.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(singleton.getInstance());
+        System.out.println(singleton.getInstance());
+        Runtime.getRuntime();
+    }
+}
